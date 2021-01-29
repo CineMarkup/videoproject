dfed20668dcc9a8fef74eac6efda827f6d22fa42
Carrie <clipsong@gmail.com>
2021-01-29 02:06:34 -0800

    Adding initial recorder component

---
+++ src/app/recorder/recorder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recorder',
+  templateUrl: './recorder.component.html',
+  styleUrls: ['./recorder.component.css']
+})
+export class RecorderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
