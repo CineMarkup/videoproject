a915d5af6b50a7ebe33b09dd6824638899349631
Carrie <clipsong@gmail.com>
2021-03-01 19:51:23 -0800

    Change routes to relative paths, using the environment variable

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -5,13 +5,15 @@ import { retry, catchError } from 'rxjs/operators';
 import { TagModel } from 'src/_models/tag-model';
 import { errorHandler } from './utilities';
 import { tap } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  hostUrl = 'http://localhost:8080/';
+  hostUrl = environment.apiUrl + 'app/';
 
   // Http Headers
   public httpOptions = {
