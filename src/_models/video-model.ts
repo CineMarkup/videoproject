488393bd514b207f37bad7ea48a029d7b11043c7
altanai <tara181989@gmail.com>
2021-03-03 16:12:22 -0800

    blob upload works

--- src/_models/video-model.ts
+++ src/_models/video-model.ts
@@ -1,11 +1,13 @@
 export interface VideoModel {
     videoID: string;
+    blob: string | Blob;
+    file: File;
     url: string;
     title: string;
     duration?: number; // calculated in annotation-editor
     tags?: Array<string>;
     maxWidth?: number; // calculated based on video width by video-player
     maxHeight?: number; // calculated based on video height by video-player
-    createdBy: string,
-    published: boolean
+    createdBy: string;
+    published: boolean;
 }
--- src/app/_services/video.service.ts
+++ src/app/_services/video.service.ts
@@ -1,15 +1,14 @@
 import {Injectable} from '@angular/core';
 import {VideoModel} from '../../_models/video-model';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
-import {TagModel} from "../../_models/tag-model";
-import { environment } from '../../environments/environment';
-
+import {HttpClient, HttpErrorResponse, HttpHeaders, HttpResponse} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {catchError, finalize, tap} from 'rxjs/operators';
+import {TagModel} from '../../_models/tag-model';
+import {environment} from '../../environments/environment';
 
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 /**
  * Video service...
@@ -23,26 +22,76 @@ export class VideoService {
 
   hostUrl = environment.apiUrl + 'app/';
 
-    // Http Headers
-    public httpOptions = {
-        headers: new HttpHeaders({
-        'Content-Type': 'application/json'
+  // Http Headers
+  public httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
+  // Http Headers for blobs
+  // 'multipart/form-data;boundary=----WebKitFormBoundaryyrV7KO0BoCBuDbTL'
+  public httpOptionsBlobs = {
+    headers: new HttpHeaders({
+      'accept-encoding': 'gzip, deflate, br',
+      connection: 'keep-alive',
+      'Content-Type': 'multipart/form-data',
+      'content-length': '1751477'
+    })
+  };
+
+  constructor(private http: HttpClient) {
+  }
+
+  public getVideos(): any {
+    return this.http.get(this.hostUrl + 'video');
+  }
+
+  public getVideoById(id: string): any {
+    return this.http.get(this.hostUrl + 'video/' + id)
+      .map(response => response as VideoModel);
+  }
+
+  public postVideo(videoModel: VideoModel): Observable<any> {
+    console.log(' video service postdata ');
+    const started = Date.now();
+    let ok: string;
+
+    const formData = new FormData();
+    // if (videoModel.file) {
+    //   formData.append('file', videoModel.file);
+    // }
+    // if (videoModel.blob){
+    //   formData.append('blob', videoModel.blob);
+    // }
+
+    formData.append('blob', videoModel.blob);
+    // videoModel
+    return this.http.post<VideoModel>(this.hostUrl + 'video', formData)
+      .pipe(
+        tap(
+          data => console.log(data),
+          error => console.log(error)
+          // Succeeds when there is a response; ignore other events
+          // event => ok = event instanceof HttpResponse ? 'succeeded' : '',
+          // // Operation failed; error is an HttpErrorResponse
+          // error => ok = 'failed'
+        ),
+        finalize(() => {
+          const elapsed = Date.now() - started;
+          console.log('Message  ', ok , elapsed);
         })
-    };
+      );
+    // .pipe(
+    //   catchError(this.handleError('postVideo', videoModel))
+    // );
+  }
 
-    constructor(private http: HttpClient) {}
 
-    public getVideos(): any {
-        return this.http.get( this.hostUrl + 'video');
-    }
-
-    public getVideoById(id: string): any {
-        return this.http.get( this.hostUrl + 'video/' + id )
-        .map(response => response as VideoModel);
-    }
-
-  public postVideo(data): Observable<any> {
-    return this.http.post<VideoModel>(this.hostUrl + 'video', data, this.httpOptions)
+  public addTag(videoID: string, tagID: string): Observable<any> {
+    const body = {tags: tagID};
+    const url = this.hostUrl + 'video/push/' + videoID;
+    return this.http.put<VideoModel>(url, JSON.stringify(body), this.httpOptions)
       .pipe(
         tap(
           data => console.log(data),
@@ -51,62 +100,51 @@ export class VideoService {
       );
   }
 
+  // tslint:disable-next-line:variable-name
+  public addDescription(videoID: string, _description: string): Observable<any> {
+    const body = {description: _description};
+    const url = this.hostUrl + 'video/' + videoID;
+    return this.http.put<VideoModel>(url, JSON.stringify(body), this.httpOptions)
+      .pipe(
+        tap(
+          data => console.log(data),
+          error => console.log(error)
+        )
+      );
+  }
 
-  public addTag(videoID: string, tagID: string): Observable<any> {
-        const body = { 'tags': tagID };
-        const url = this.hostUrl + 'video/push/' + videoID;
-        return this.http.put<VideoModel>(url, JSON.stringify(body), this.httpOptions)
-        .pipe(
-          tap(
-            data => console.log(data),
-            error => console.log(error)
-          )
-        );
-    }
+  public addTitle(videoID: string, titleToBeAdded: string): Observable<any> {
+    const body = {title: titleToBeAdded};
+    const url = this.hostUrl + 'video/' + videoID;
+    return this.http.put<VideoModel>(url, JSON.stringify(body), this.httpOptions)
+      .pipe(
+        tap(
+          data => console.log(data),
+          error => console.log(error)
+        )
+      );
+  }
 
-    public addDescription(videoID: string, description: string): Observable<any> {
-        const body = { 'description': description };
-        const url = this.hostUrl + 'video/' + videoID;
-        return this.http.put<VideoModel>(url, JSON.stringify(body), this.httpOptions)
-        .pipe(
-          tap(
-            data => console.log(data),
-            error => console.log(error)
-          )
-        );
-    }
+  // tslint:disable-next-line:variable-name
+  public addPublishedDate(videoID: string, _published: string) {
+    const today = new Date().toString();
+    const body = {published: _published, publishedAt: today};
+    const url = this.hostUrl + 'video/' + videoID;
+    return this.http.put<any>(url, JSON.stringify(body), this.httpOptions)
+      .subscribe((r) => {
+        console.log(r);
+      });
+  }
 
-    public addTitle(videoID: string, title: string): Observable<any> {
-        const body = { 'title': title };
-        const url = this.hostUrl + 'video/' + videoID;
-        return this.http.put<VideoModel>(url, JSON.stringify(body), this.httpOptions)
-        .pipe(
-          tap(
-            data => console.log(data),
-            error => console.log(error)
-          )
-        );
-    }
-
-    public addPublishedDate(videoID: string, published: string) {
-        const today = new Date().toString();
-        const body = { 'published': published, 'publishedAt': today };
-        const url = this.hostUrl + 'video/' + videoID;
-        return this.http.put<any>(url, JSON.stringify(body), this.httpOptions)
-        .subscribe((r) => {
-          console.log(r);
-        });
-    }
-
-    public deleteTagFromList(videoID: string, tagID: string): Observable<any> {
-      const url = this.hostUrl + 'video/' + videoID + '/remove/' + tagID;
-      return this.http.put<VideoModel>(url, {}, this.httpOptions)
+  public deleteTagFromList(videoID: string, tagID: string): Observable<any> {
+    const url = this.hostUrl + 'video/' + videoID + '/remove/' + tagID;
+    return this.http.put<VideoModel>(url, {}, this.httpOptions)
       .pipe(
         tap(
           data => console.log(data),
           error => console.error(error)
         )
       );
-    }
+  }
 
 }
