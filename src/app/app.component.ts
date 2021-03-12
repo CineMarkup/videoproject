a9de9ad991b412dfc62c381ce429a3de99f4bb82
loydp <54460504+loydp@users.noreply.github.com>
2021-03-12 05:29:38 -0800

    Navbar now shows profile elements on login.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
+// import { userInfo } from 'os';
 import {LoginService} from '../app/_services/login.service';
+import { UserService } from './_services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +13,36 @@ export class AppComponent {
   loggedIn = false;
   email: any = 'anon@gmail.com';
   name: any = 'Logged-in';
+  avatar: any = 'default';
 
   constructor(loginService: LoginService) {
     loginService.getLogin().subscribe(res => {
-      this.loggedIn = res.loggedIn;
-      this.email = res.loggedIn;
-      console.log("LOGGED IN " + res.loggedIn);
-    });
+      this.loggedIn = true;
+      this.email = res.email;
+      this.name = res.displayName;
+      this.avatar = res.avatar;
+      /* ABOUT res fields:
+        res.
+          _id = mongodb id (BAD FORM TO USE THIS)
+          userID = 'u192'
+          googleID = 'numeric string I think'
+          avatar = 'url'
+          displayName = 'user chosen display name'
+          email = 'string'
+          userType = 'user'
+      */
+    },
+    error => {
+      // server sends back 403 on unauthenticated getLogin()
+      if (error.status == 403) {
+        this.loggedIn = false;
+      }
+    }
+    );
+  }
+
+  // BACKLOG: placeholder stub. Currently does nothing.
+  public logout() {
+    this.loggedIn = false;
   }
 }
